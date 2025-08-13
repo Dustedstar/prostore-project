bf8ad76b018fcde87804d32bf1d72146d64a778d
Mehryar <gargoyle.junior@gmail.com>
2025-08-14 02:00:45 +0330

    Commenting out :any

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,15 @@
 import { APP_NAME } from "@/lib/constants";
-import sampleData from "@/db/sample-data";
-import ProductList from "@/components/shared/product/product-list";
+// import sampleData from "@/db/sample-data";
+// import ProductList from "@/components/shared/product/product-list";
 
 export default function Home() {
   return (
     <>
-      <ProductList
+      {/* <ProductList
         data={sampleData.products}
         title="Newest Arrivals"
         limit={4}
-      />
+      /> */}
     </>
   );
 }
