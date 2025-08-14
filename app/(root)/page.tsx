e11d0ea14966732334bd3e65b8981444a04bfa9b
Mehryar <gargoyle.junior@gmail.com>
2025-08-14 21:25:40 +0330

    Fixing TypeScript bug

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,12 @@ import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
 
 export default async function Home() {
-  const latestProducts = await getLatestProducts();
+  const latestProductsRaw = await getLatestProducts();
+  const latestProducts = latestProductsRaw.map((product) => ({
+    ...product,
+    price: product.price.toString(),
+    rating: product.rating.toString(),
+  }));
   return (
     <>
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
